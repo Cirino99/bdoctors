f628b1ccc2dc2bb541a63cc827f503068f8a79c2
mouhcine <mouhcine15.00@gmail.com>
2022-09-09 17:51:55 +0200

    review seeder

--- database/seeds/ReviewSeeder.php
+++ database/seeds/ReviewSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class ReviewSeeder extends Seeder
@@ -9,8 +11,19 @@ class ReviewSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $users_ids = User::all()->pluck('id');
+
+        for ($i = 0; $i < 20; $i++) {
+            $review = new Review;
+
+            $review->review = $faker->paragraphs(3);
+            $review->vote = $faker->numberBetween(1,5);
+            $review->date = $faker->date('d_m_y');
+            $review->date = $faker->firstName();
+
+            $review->save();
+        }
     }
 }
