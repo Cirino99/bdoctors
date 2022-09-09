a3331f7bf99e5dde3ebe6329e3d7e4a57be6a3f5
DanieleKun <pucciodaniele95@gmail.com>
2022-09-09 16:34:37 +0200

    migration users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,17 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('lastname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('address');
             $table->string('password');
+            $table->string('phone');
+            $table->string('city');
+            $table->mediumInteger('postal_code');
+            $table->text('cv');
+            $table->string('service');
+            $table->string('photo');
             $table->rememberToken();
             $table->timestamps();
         });
