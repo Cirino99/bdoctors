a6eccb9ff8b40593a9a63d568c73c2a52e1456e4
Cirino99 <cirino.simone99@gmail.com>
2022-09-12 11:47:08 +0200

    modifica model sponsorship

--- app/Models/Sponsorship.php
+++ app/Models/Sponsorship.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sponsorship extends Model
 {
-    //
+    protected $fillable = ['price', 'time'];
+
+    public function users()
+    {
+        return $this->belongsToMany('App\Models\User')->withPivot('user_id', 'starting_date', 'ending_date', 'id_paymant');
+    }
 }
