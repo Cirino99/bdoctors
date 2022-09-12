a80100f934892fc1327cd7051f3323ca020da304
KSigma91 <andrea19912014@gmail.com>
2022-09-12 17:11:08 +0200

    modifica ai models User e Message

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
     protected $fillable = ['user_id', 'message', 'date', 'email', 'sender_receiver'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
     {
         return $this->belongsToMany('App\Models\Specialization');
     }
+
+    public function messages()
+    {
+        return $this->hasMany('App\Models\Message');
+    }
 }
