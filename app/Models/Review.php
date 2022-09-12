e3dc5baa6d95efa40ee2238448b5c394a1737ac7
mouhcine <mouhcine15.00@gmail.com>
2022-09-12 17:12:44 +0200

    belongsTo Review

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -9,4 +9,7 @@ class Review extends Model
     protected $fillable = [
         'review', 'vote', 'date','name', 'user_id'
     ];
+    public function user() {
+        return $this->belongsTo('App\Models\User');
+    }
 }
