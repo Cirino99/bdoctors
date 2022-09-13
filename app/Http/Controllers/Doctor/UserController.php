706e75f3da5575d43483aa632dfbead6365a7b86
mouhcine <mouhcine15.00@gmail.com>
2022-09-13 18:50:14 +0200

    aggiornamento UserController

--- app/Http/Controllers/Doctor/UserController.php
+++ app/Http/Controllers/Doctor/UserController.php
@@ -29,37 +29,6 @@ class UserController extends Controller
     ];
 
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $users = User::paginate(15);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $request->validate($this->validation_rules);
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param  \App\Models\User  $user
