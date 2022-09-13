82b87c1b14d87771cfaff18521a49b19836b2785
mouhcine <mouhcine15.00@gmail.com>
2022-09-13 17:52:14 +0200

    edit a update da completare

--- app/Http/Controllers/Doctor/UserController.php
+++ app/Http/Controllers/Doctor/UserController.php
@@ -11,6 +11,20 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
+
+    protected $validation_rules = [
+        'name'              => 'required|string|max:255',
+        'lastname'          => 'required|string|max:255',
+        // 'email'             => 'required|string|max:255|unique:users|exists:users,email',
+        'photo'             => 'string|max:255',
+        'phone'             => 'string|max:255',
+        'service'           => 'required|string|max:255',
+        'address'           => 'required|string|max:255',
+        'city'              => 'required|string|max:255',
+        'postal_code'       => 'required|integer',
+        // 'cv'             =>'text'
+    ];
+
     /**
      * Display a listing of the resource.
      *
@@ -39,7 +53,7 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->validation_rules);
     }
 
     /**
@@ -74,7 +88,20 @@ class UserController extends Controller
      */
     public function edit(int $profile)
     {
-        return view('doctor.profile.edit');
+        if ($profile === Auth::id()) {
+            //$user = User::where('id', $profile);
+            $users = User::all();
+            foreach ($users as $user) {
+                if ($user->id === $profile) {
+                    $myUser = $user;
+                }
+            }
+            return view('doctor.profile.edit', [
+                'user' => $myUser
+            ]);
+        } else {
+            return view('doctor.dashboard');
+        }
     }
 
     /**
@@ -84,9 +111,17 @@ class UserController extends Controller
      * @param  \App\Models\User  $user
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, User $user)
+    public function update(Request $request, User $profile)
     {
-        //
+        $request->validate($this->validation_rules);
+
+        $data = $request->all();
+
+        $profile->update($data);
+
+        // $user->save();
+
+        return redirect()->route('doctor.profile.show', $profile);
     }
 
     /**
--- resources/views/doctor/profile/edit.blade.php
+++ resources/views/doctor/profile/edit.blade.php
@@ -15,6 +15,152 @@
                     @endif
 
                     <h1>Edit</h1>
+                    <form action="{{ route('doctor.profile.update', ['profile' => $user]) }}" method="post" novalidate enctype="multipart/form-data">
+                        @csrf
+                        @method('put')
+                
+                        <div class="mb-3">
+                            <label class="form-label" for="name">Nome</label>
+                            <input class="form-control @error('name') is-invalid @enderror"
+                                type="text"
+                                name="name"
+                                id="name"
+                                value="{{ old('name', $user->name) }}"
+                            >
+                            @error('name')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label class="form-label" for="lastname">Cognome</label>
+                            <input class="form-control @error('lastname') is-invalid @enderror"
+                                type="text"
+                                name="lastname"
+                                id="lastname"
+                                value="{{ old('lastname', $user->lastname) }}"                            >
+                            @error('lastname')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        {{-- <div class="mb-3">
+                            <label class="form-label" for="email">Email</label>
+                            <input class="form-control @error('email') is-invalid @enderror"
+                                type="text"
+                                name="email"
+                                id="email"
+                                value="{{ old('email', $user->email) }}"                            >
+                            @error('email')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div> --}}
+                
+                        <div class="mb-3">
+                            <label class="form-label" for="photo">Photo</label>
+                            <input class="form-control @error('photo') is-invalid @enderror"
+                                type="text"
+                                name="photo"
+                                id="photo"
+                                {{-- value="{{ old('name', $user->name) }}"                             --}}
+                                >
+                            @error('photo')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label class="form-label" for="phone">Numero di telefono</label>
+                            <input class="form-control @error('phone') is-invalid @enderror"
+                                type="text"
+                                name="phone"
+                                id="phone"
+                                 {{-- value="{{ old('name', $user->name) }}"                             --}}
+                                > 
+                            @error('phone')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label class="form-label" for="service">Prestazione</label>
+                            <input class="form-control @error('service') is-invalid @enderror"
+                                type="text"
+                                name="service"
+                                id="service"
+                                {{-- value="{{ old('name', $user->name) }}"                             --}}
+                                >
+                            @error('service')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label class="form-label" for="address">Via</label>
+                            <input class="form-control @error('address') is-invalid @enderror"
+                                type="text"
+                                name="address"
+                                id="address"
+                                value="{{ old('address', $user->address) }}"                            >
+                            @error('address')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label class="form-label" for="city">Città</label>
+                            <input class="form-control @error('city') is-invalid @enderror"
+                                type="text"
+                                name="city"
+                                id="city"
+                                value="{{ old('city', $user->city) }}"                            >
+                            @error('city')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label class="form-label" for="postal_code">Codice Postale</label>
+                            <input class="form-control @error('postal_code') is-invalid @enderror"
+                                type="text"
+                                name="postal_code"
+                                id="postal_code"
+                                value="{{ old('postal_code', $user->postal_code) }}"                            >
+                            @error('postal_code')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                
+                        {{-- <div class="mb-3">
+                            <label class="form-label" for="excerpt">Excerpt</label>
+                            <textarea class="form-control @error('excerpt') is-invalid @enderror" name="excerpt" id="excerpt">{{ old('excerpt', $post->excerpt) }}</textarea>
+                            @error('excerpt')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div> --}}
+                
+                       <button type="submit" class="btn btn-primary">Save</button>
+                    </form>
                 </div>
             </div>
         </div>
