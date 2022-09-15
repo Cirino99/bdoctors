0ea3cd6395df90595ddfb971dce93aea8226bed6
mouhcine <mouhcine15.00@gmail.com>
2022-09-15 16:50:02 +0200

    pageHome inizializzazione

--- resources/js/front.js
+++ resources/js/front.js
@@ -3,13 +3,22 @@ require('./bootstrap');
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App.vue';
+import Home from './pages/PageHome.vue';
 
-const routes = [];
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: Home,
+    }
+];
 
 const router = new VueRouter({
     routes,
     mode: 'history',
 });
+
+Vue.use(VueRouter);
 
 const app = new Vue({
     el: '#root',
