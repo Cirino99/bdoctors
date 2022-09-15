29dd6143bd0d57d278e83c13555516d75bdb9837
mouhcine <mouhcine15.00@gmail.com>
2022-09-15 22:47:10 +0200

    PageHome chiamata axios funzionante

--- resources/js/pages/PageHome.vue
+++ resources/js/pages/PageHome.vue
@@ -14,15 +14,15 @@
         </h3>
       </div>
       <div class="d-flex justify-content-center">
-        <div class="card" style="width: 18rem;">
+        <div v-for="doctor in doctors" class="card" style="width: 18rem;">
           <img class="card-img-top" src="" alt="Card image cap">
           <div class="card-body">
-            <h5 class="card-title"> {{ doctors.name }} </h5>
-            <h5 class="card-title">Cognome</h5>
+            <h5 class="card-title"> {{ doctor.name }} </h5>
+            <h5 class="card-title"> {{ doctor.lastname }} </h5>
             <p class="card-text">Testo</p>
           </div>
           <ul class="list-group list-group-flush">
-            <li class="list-group-item">SPECIALIZZAZIONE</li>
+            <li class="list-group-item"> {{ doctor.specialization }} </li>
             <li class="list-group-item">VALUTAZIONE</li>
           </ul>
           <div class="card-body">
@@ -52,10 +52,10 @@
     created() {
       axios.get('/api/doctors')
         .then(res => {
-          if (res.data.success) {
-            this.doctors = res.data.result
-          }
-        })     
+            if (res.data.success) {
+              this.doctors = res.data.result
+            }
+          })
     }
   }
 </script>
