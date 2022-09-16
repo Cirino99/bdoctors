6b09c253771c694575a44be8c219cc5fb0a4c438
KSigma91 <andrea19912014@gmail.com>
2022-09-16 12:48:08 +0200

    aggiunta chiamata axios in PageShow

--- resources/js/pages/PageShow.vue
+++ resources/js/pages/PageShow.vue
@@ -1,26 +1,39 @@
 <template>
   <div class="container mt-5">
         <div class="d-flex justify-content-between mb-5">
-            <div class="row">
-                <ul>
-                    <li>questo è un testo</li>
-                    <li>questo è un testo</li>
-                    <li>questo è un testo</li>
-                    <li>questo è un testo</li>
-                    <li>questo è un testo</li>
-                    <li>questo è un testo</li>
-                    <li>questo è un testo</li>
-                    <li>questo è un testo</li>
-                    <li>questo è un testo</li>
-                    <li>questo è un testo</li>
-                    <li>questo è un testo</li>
-                    <li>questo è un testo</li>
+            <div class="col-6 col-sm-6 col-md-6 col-lg-6">
+                <ul class="d-flex flex-column gap-2">
+                    <li>
+                        <b class="fst-italic">Nome: </b> {{ showProfile.name }}
+                    </li>
+                    <li>
+                        <b class="fst-italic">Cognome: </b> {{ showProfile.lastname }}
+                    </li>
+                    <li>
+                        <b class="fst-italic">Email: </b> {{ showProfile.email }}
+                    </li>
+                    <li>
+                        <b class="fst-italic">Via: </b> {{ showProfile.address }}
+                    </li>
+                    <li>
+                        <b class="fst-italic">Numero di telefono: </b> {{ showProfile.phone }}
+                    </li>
+                    <li>
+                        <b class="fst-italic">Città: </b> {{ showProfile.city }}
+                    </li>
+                    <li>
+                        <b class="fst-italic">CAP: </b> {{ showProfile.postal_code }}
+                    </li>
+                    <li>
+                        <b class="fst-italic">Specializzazione: </b>
+                        <span v-for="specialization in showProfile.specializations" :key="specialization.id">
+                            {{ specialization.name }}
+                        </span>
+                    </li>
                 </ul>
             </div>
-            <div class="row d-flex justify-content-center align-items-center">
-                <div class="col-12 col-sm-12 col-md-12 col-lg-12">
-                    <img class="img-fluid img-thumbnail rounded-circle" src="https://www.limontasport.com/wp-content/uploads/2016/03/landscape-test.jpg" alt="">
-                </div>
+            <div class="col-6 col-sm-4 col-md-4 col-lg-4">
+                <img class="img-fluid img-thumbnail rounded-circle" :src="showProfile.photo" :alt="showProfile.name">
             </div>
         </div>
         <div class="d-flex justify-content-between align-items-center">
@@ -31,8 +44,24 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    name: 'PageShow'
+    name: 'PageShow',
+    data() {
+        return {
+            showProfile: []
+        }
+    },
+    created() {
+        axios.get('/api/doctors/2')
+            .then(res =>{
+                if (res.data.success) {
+                    this.showProfile = res.data.result
+                    console.log(this.showProfile);
+                }
+            })
+    }
 }
 </script>
 
