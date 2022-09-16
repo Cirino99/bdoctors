424caffa8d8a3a04dcd9c960fcaba9a3358e5103
Cirino99 <cirino.simone99@gmail.com>
2022-09-16 17:16:46 +0200

    prima versione api DoctorController

--- app/Http/Controllers/Api/DoctorController.php
+++ app/Http/Controllers/Api/DoctorController.php
@@ -61,4 +61,87 @@ class DoctorController extends Controller
             ]);
         }
     }
+
+    public function specialization(Request $request)
+    {
+        $specialization = $request->get('specialization');
+        $specializations = DB::table('specializations')
+            ->where('name', 'like', '%' . $specialization . '%')
+            ->select('id', 'name')
+            ->get();
+        if ($specializations) {
+            return response()->json([
+                'success'   => true,
+                'result'    => $specializations
+            ]);
+        } else {
+            return response()->json([
+                'success'   => false,
+            ]);
+        }
+    }
+
+    public function search(Request $request)
+    {
+        $specialization = $request->get('specialization');
+        $city = $request->get('city');
+        //$search = User::with(['specializations'])->where('specializations', 'like', $data)->first();
+        //$search = User::where('id', 'like', "%{$data}%")->get();
+        if ($city == 'all') {
+            $doctors_ids = DB::table('users')
+                ->join('specialization_user', 'users.id', '=', 'specialization_user.user_id')
+                ->where('specialization_user.specialization_id', 'like', $specialization)
+                ->select('users.id')
+                ->get();
+        } else {
+            $doctors_ids = DB::table('users')
+                ->join('specialization_user', 'users.id', '=', 'specialization_user.user_id')
+                ->where('users.city', 'like', $city)
+                ->where('specialization_user.specialization_id', 'like', $specialization)
+                ->select('users.id')
+                ->get();
+        }
+
+        //dump($specialization, $city, $doctors_ids);
+        $doctors = [];
+        foreach ($doctors_ids as $doctor_id) {
+            $doctor = User::with(['specializations'])->where('id', $doctor_id->id)->first();
+            unset($doctor->email_verified_at, $doctor->created_at, $doctor->updated_at);
+            foreach ($doctor->specializations as $specialization) {
+                unset($specialization->created_at, $specialization->updated_at, $specialization->pivot);
+            }
+            $doctor->photo = $this->fixImageUrl($doctor->photo);
+            array_push($doctors, $doctor);
+        }
+        if ($doctors) {
+            return response()->json([
+                'success'   => true,
+                'result'    => $doctors
+            ]);
+        } else {
+            return response()->json([
+                'success'   => false,
+            ]);
+        }
+    }
+
+    public function city(Request $request)
+    {
+        $city = $request->get('city');
+        $citys = DB::table('users')
+            ->where('city', 'like', '%' . $city . '%')
+            ->groupBy('city')
+            ->select('city')
+            ->get();
+        if ($citys) {
+            return response()->json([
+                'success'   => true,
+                'result'    => $citys
+            ]);
+        } else {
+            return response()->json([
+                'success'   => false,
+            ]);
+        }
+    }
 }
