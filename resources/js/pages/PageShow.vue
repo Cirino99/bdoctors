4adae21cc66f3701c742f0a40dd7bda0daa0023f
KSigma91 <andrea19912014@gmail.com>
2022-09-16 10:53:49 +0200

    modifica PageShow ancora incompleta

--- resources/js/pages/PageShow.vue
+++ resources/js/pages/PageShow.vue
@@ -1,10 +1,27 @@
 <template>
   <div class="container mt-5">
         <div class="d-flex justify-content-between mb-5">
-            <ul class="col-2">
-                <li></li>
-            </ul>
-            <img class="col-4 img-fluid img-thumbnail rounded-circle" src="https://www.limontasport.com/wp-content/uploads/2016/03/landscape-test.jpg" alt="">
+            <div class="row">
+                <ul>
+                    <li>questo è un testo</li>
+                    <li>questo è un testo</li>
+                    <li>questo è un testo</li>
+                    <li>questo è un testo</li>
+                    <li>questo è un testo</li>
+                    <li>questo è un testo</li>
+                    <li>questo è un testo</li>
+                    <li>questo è un testo</li>
+                    <li>questo è un testo</li>
+                    <li>questo è un testo</li>
+                    <li>questo è un testo</li>
+                    <li>questo è un testo</li>
+                </ul>
+            </div>
+            <div class="row d-flex justify-content-center align-items-center">
+                <div class="col-12 col-sm-12 col-md-12 col-lg-12">
+                    <img class="img-fluid img-thumbnail rounded-circle" src="https://www.limontasport.com/wp-content/uploads/2016/03/landscape-test.jpg" alt="">
+                </div>
+            </div>
         </div>
         <div class="d-flex justify-content-between align-items-center">
             <button class="btn btn-secondary">Contatta</button>
@@ -25,6 +42,11 @@ export default {
     }
 
     img {
-        width: 300px;
+        min-width: 150px;
+        min-height: 150px;
+        width: 30vw;
+        height: 30vw;
+        object-fit: cover;
+        object-position: center;
     }
 </style>
