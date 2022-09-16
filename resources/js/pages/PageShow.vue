dd315164d61aa81f90708414b98feeec0d3b9e8e
KSigma91 <andrea19912014@gmail.com>
2022-09-16 16:53:32 +0200

    aggiunto responsive nella PageShow

--- resources/js/pages/PageShow.vue
+++ resources/js/pages/PageShow.vue
@@ -1,44 +1,50 @@
 <template>
   <div class="container mt-5">
-        <div class="d-flex justify-content-between mb-5">
-            <div class="col-6 col-sm-6 col-md-6 col-lg-6">
-                <ul class="d-flex flex-column gap-2">
-                    <li>
-                        <b class="fst-italic">Nome: </b> {{ showProfile.name }}
-                    </li>
-                    <li>
-                        <b class="fst-italic">Cognome: </b> {{ showProfile.lastname }}
-                    </li>
-                    <li>
-                        <b class="fst-italic">Email: </b> {{ showProfile.email }}
-                    </li>
-                    <li>
-                        <b class="fst-italic">Via: </b> {{ showProfile.address }}
-                    </li>
-                    <li>
-                        <b class="fst-italic">Numero di telefono: </b> {{ showProfile.phone }}
-                    </li>
-                    <li>
-                        <b class="fst-italic">Città: </b> {{ showProfile.city }}
-                    </li>
-                    <li>
-                        <b class="fst-italic">CAP: </b> {{ showProfile.postal_code }}
-                    </li>
-                    <li>
-                        <b class="fst-italic">Specializzazione: </b>
-                        <span v-for="specialization in showProfile.specializations" :key="specialization.id">
-                            {{ specialization.name }}
-                        </span>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-6 col-sm-4 col-md-4 col-lg-4">
-                <img class="img-fluid img-thumbnail rounded-circle" :src="showProfile.photo" :alt="showProfile.name">
+        <div class="d-flex justify-content-between flex-wrap mb-5">
+            <!-- lista info profilo -->
+            <ul class="col-12 col-sm-12 col-md-6 col-lg-6 d-flex flex-column justify-content-center gap-2 order-2">
+                <li>
+                    <b class="fst-italic">Nome: </b> {{ showProfile.name }}
+                </li>
+                <li>
+                    <b class="fst-italic">Cognome: </b> {{ showProfile.lastname }}
+                </li>
+                <li>
+                    <b class="fst-italic">Email: </b> {{ showProfile.email }}
+                </li>
+                <li>
+                    <b class="fst-italic">Via: </b> {{ showProfile.address }}
+                </li>
+                <li>
+                    <b class="fst-italic">Numero di telefono: </b> {{ showProfile.phone }}
+                </li>
+                <li>
+                    <b class="fst-italic">Città: </b> {{ showProfile.city }}
+                </li>
+                <li>
+                    <b class="fst-italic">CAP: </b> {{ showProfile.postal_code }}
+                </li>
+                <li>
+                    <b class="fst-italic">Specializzazione: </b>
+                    <span v-for="specialization in showProfile.specializations" :key="specialization.id"
+                    class="badge bg-warning mx-1">
+                        {{ specialization.name }}
+                    </span>
+                </li>
+            </ul>
+            <!-- foto profilo -->
+            <div class="col-12 col-sm-12 col-md-6 d-flex justify-content-center order-md-2">
+                <img class="img-fluid img-thumbnail rounded-circle mb-4" :src="showProfile.photo" :alt="showProfile.name">
             </div>
         </div>
         <div class="d-flex justify-content-between align-items-center">
-            <button class="btn btn-secondary">Contatta</button>
-            <button class="btn btn-success">Scrivi una recensione</button>
+            <button class="btn btn-secondary">Prenota</button>
+            <button class="btn" style="background: #23A3B3; color: #fff">Scrivi una recensione</button>
+        </div>
+        <!-- campo per la recensione temporaneo TODO: da sistemare -->
+        <div class="form-floating my-4">
+            <textarea class="form-control" placeholder="Lascia una recensione" id="floatingTextarea2" style="height: 100px"></textarea>
+            <label for="floatingTextarea2" style="color: #cecece">Digita qui..</label>
         </div>
     </div>
 </template>
@@ -68,6 +74,10 @@ export default {
 <style lang="scss" scoped>
     li {
         list-style-type: none;
+
+        b {
+            color: #008ecf;
+        }
     }
 
     img {
