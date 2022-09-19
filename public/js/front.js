f667a82fd3f1fa4d0ca73a02d4e81c629d1e59aa
KSigma91 <andrea19912014@gmail.com>
2022-09-19 18:36:45 +0200

    menù a tendina della barra search di PageHome

--- public/js/front.js
+++ public/js/front.js
@@ -20370,7 +20370,7 @@ __webpack_require__.r(__webpack_exports__);
     searchDoctor: function searchDoctor() {
       var _this2 = this;
 
-      axios.get('api/search?specialization=' + this.specializationSelect + '&city=all&reviews=0&vote=1').then(function (res) {
+      axios.get('api/search?specialization=' + this.specializationSelect + '&city=all&reviews= ' + this.review + '  &vote=' + this.vote).then(function (res) {
         if (res.data.success) {
           _this2.doctors = res.data.result;
         }
@@ -20378,6 +20378,14 @@ __webpack_require__.r(__webpack_exports__);
     },
     changeSpecialization: function changeSpecialization(id) {
       this.specializationSelect = id;
+      this.searchDoctor();
+    },
+    changeVote: function changeVote(id) {
+      this.vote = id;
+      this.searchDoctor();
+    },
+    changeReview: function changeReview(id) {
+      this.review = id;
       this.searchDoctor();
     }
   }
@@ -20705,6 +20713,11 @@ var render = function render() {
       },
       domProps: {
         checked: _vm.vote === item
+      },
+      on: {
+        click: function click($event) {
+          return _vm.changeVote(item);
+        }
       }
     }), _vm._v(" "), _c("label", {
       staticClass: "form-check-label",
@@ -20726,6 +20739,11 @@ var render = function render() {
       },
       domProps: {
         checked: _vm.review === item
+      },
+      on: {
+        click: function click($event) {
+          return _vm.changeReview(item);
+        }
       }
     }), _vm._v(" "), _c("label", {
       staticClass: "form-check-label",
@@ -20821,11 +20839,13 @@ var render = function render() {
       alt: "lens-search"
     }
   })])], 1)]), _vm._v(" "), _vm.display ? _c("div", {
-    staticClass: "d-flex justify-content-center",
+    staticClass: "collapse position-absolute top- d-flex justify-content-center",
     on: {
       mouseleave: _vm.handleFocusOut
     }
-  }, [_c("ul", _vm._l(_vm.specializations, function (specialization) {
+  }, [_c("ul", {
+    staticClass: "card overflow-auto"
+  }, _vm._l(_vm.specializations, function (specialization) {
     return _c("li", {
       key: specialization.id,
       on: {
@@ -20842,7 +20862,7 @@ var render = function render() {
       color: "#00334e"
     }
   }, [_vm._v("\n                        MEDICI SPONSORIZZATI\n                    ")])], 1), _vm._v(" "), _c("div", {
-    staticClass: "d-flex justify-content-center gap-5 my-5"
+    staticClass: "d-flex flex-wrap justify-content-center gap-5 my-5"
   }, _vm._l(_vm.doctors, function (doctor, index) {
     return _c("CardDoctor", {
       key: index,
@@ -26352,7 +26372,7 @@ exports = module.exports = __webpack_require__(/*! ../../../node_modules/css-loa
 
 
 // module
-exports.push([module.i, "#jumbotron[data-v-13e03f97] {\n  min-width: 100%;\n  min-height: 150px;\n  width: 20vw;\n  height: 30vw;\n  -o-object-fit: cover;\n     object-fit: cover;\n  -o-object-position: center;\n     object-position: center;\n}\n#search-button[data-v-13e03f97] {\n  width: 100px;\n}\n#search-icon[data-v-13e03f97] {\n  max-width: 20px;\n}\nli[data-v-13e03f97]:hover {\n  background-color: aquamarine;\n}", ""]);
+exports.push([module.i, "#jumbotron[data-v-13e03f97] {\n  min-width: 100%;\n  min-height: 150px;\n  width: 20vw;\n  height: 30vw;\n  -o-object-fit: cover;\n     object-fit: cover;\n  -o-object-position: center;\n     object-position: center;\n}\n#search-button[data-v-13e03f97] {\n  width: 100px;\n}\n#search-icon[data-v-13e03f97] {\n  max-width: 20px;\n}\nli[data-v-13e03f97] {\n  list-style-type: none;\n}\nli[data-v-13e03f97]:hover {\n  background-color: aquamarine;\n}", ""]);
 
 // exports
 
@@ -60501,8 +60521,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\DanielePC\Desktop\Boolean\Final Project\bdoctors\resources\js\front.js */"./resources/js/front.js");
-module.exports = __webpack_require__(/*! C:\Users\DanielePC\Desktop\Boolean\Final Project\bdoctors\resources\sass\back.scss */"./resources/sass/back.scss");
+__webpack_require__(/*! C:\Users\Administrator\Documents\VSC\Progetto-Finale\bdoctors\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\Users\Administrator\Documents\VSC\Progetto-Finale\bdoctors\resources\sass\back.scss */"./resources/sass/back.scss");
 
 
 /***/ })
