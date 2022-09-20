2aa9320504b82ab2661de332ad520f5e5a6c98a2
mouhcine <mouhcine15.00@gmail.com>
2022-09-20 17:40:49 +0200

    Footer link da completare

---
+++ resources/js/pages/PageContactUs.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        CONTATTACI
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'PageContactUs',
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+    // colors
+
+    
+    .blu-scuro {
+        color: #00acff;
+    }
+    .blu-chiaro {
+        color: #00c7ff;
+    }
+    .blu-chiaro2 {
+        color: #8ce6ff;
+    }
+    .nero {
+        color: #2a2d45;
+    }
+    .bg-blu-scuro {
+        background-color: #00acff;
+    }
+    .bg-blu-chiaro {
+        background-color: #00c7ff;
+    }
+    .bg-blu-chiaro2 {
+        background-color: #8ce6ff;
+    }
+    .bg-nero {
+        background-color: #2a2d45;
+    }
+    
+</style>
