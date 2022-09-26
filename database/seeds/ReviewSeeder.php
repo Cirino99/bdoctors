3376f15e1db33301aed33bd33e0e674ac22b751a
DanieleKun <pucciodaniele95@gmail.com>
2022-09-26 12:55:18 +0200

    fix seeder messaggi e recensioni

--- database/seeds/ReviewSeeder.php
+++ database/seeds/ReviewSeeder.php
@@ -14,15 +14,21 @@ class ReviewSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $myReview = 'Ottimo medico, serio, competente è gentile come pochi';
+
         $users_ids = User::all()->pluck('id');
         foreach ($users_ids as $user_id) {
-            $review = new Review;
-            $review->user_id = $user_id;
-            $review->review = implode($faker->paragraphs(3));
-            $review->vote = $faker->numberBetween(1, 5);
-            $review->date = $faker->date('Y_m_d');
-            $review->name = $faker->firstName();
-            $review->save();
+            $nReviews = $faker->numberBetween(1, 10);
+            for ($i=0; $i < $nReviews; $i++) { 
+                $review = new Review;
+                $review->user_id = $user_id;
+                // $review->review = implode($faker->paragraphs(3));
+                $review->review = $myReview;
+                $review->vote = $faker->numberBetween(1, 5);
+                $review->date = $faker->dateTimeThisYear();
+                $review->name = $faker->firstName();
+                $review->save();
+            }
         }
     }
 }
